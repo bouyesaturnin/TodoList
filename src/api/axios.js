efb03c086ccf65dc2ff21e94a62df6c0d1d1ee8d
Saturnin <bouyesaturnin@yahoo.fr>
2026-02-12 05:10:54 +0100

    Correction : Utilisation de la vraie URL API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://missions-hub-api-5.onrender.com/api/',
+  baseURL: process.env.REACT_APP_API_URL || 'https://missions-hub-api-5.onrender.com/api',
 });
 
 api.interceptors.request.use(
